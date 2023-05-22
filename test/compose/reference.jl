43ad33ee518a0926d75cd7b0ff02776db2b54c31
Rob Smit <rnsmit@gmail.com>
2023-05-22 20:38:10 +0200

    rewrite test

--- test/compose/reference.jl
+++ test/compose/reference.jl
@@ -1,21 +1,37 @@
 @info "Running reference tests"
 @testset "reference" begin
-    persontable = TableSource(:person, :id => SQLCompose.Int8Type, :first_name => SQLCompose.TextType, :initials => SQLCompose.TextType, :surname => SQLCompose.TextType)
-    salarytable = TableSource(:salary, :person_id => SQLCompose.Int8Type, :amount => SQLCompose.Int4Type)
+    persons = TableSource(:person, :id => SQLCompose.Int8Type,
+        :first_name => SQLCompose.TextType,
+        :initials => SQLCompose.TextType,
+        :surname => SQLCompose.TextType)
+    salaries = TableSource(:salary,
+        :person_id => SQLCompose.Int8Type,
+        :amount => SQLCompose.Int4Type)
 
     fullname(p) = p.first_name * " " * p.surname
-    personsalary(p) = reference(salarytable, (:person_id,), (p.id,))
+    salary(p) = reference(salaries, (:person_id,), (p.id,))
 
     @info "test without reference"
-    @testsql map((p, s) -> (person=fullname(p), salary=s.amount),
-        join((p, s) -> p.id == s.person_id,
-            query(persontable), query(salarytable))),
+    @testsql (@query persons begin
+        join(_, salaries) do p, s
+            p.id == s.person_id
+        end
+        map(_) do p, s
+            (person=fullname(p), salary=s.amount)
+        end
+    end),
     "SELECT CONCAT(p.first_name, ' ', p.surname) AS person, 
         s.amount AS salary FROM person p INNER JOIN salary s ON p.id = s.person_id"
 
     @info "test with reference"
-    @testsql map(p -> (person=fullname(p), salary=personsalary(p).amount), query(persontable)),
+    @testsql (@query persons begin
+        map(_) do p
+            (person=fullname(p), salary=salary(p).amount)
+        end
+    end),
     "SELECT CONCAT(p.first_name, ' ', p.surname) AS person, ref_salary.amount AS salary FROM person p 
         INNER JOIN salary ref_salary ON p.id = ref_salary.person_id"
+
+
 end
 
